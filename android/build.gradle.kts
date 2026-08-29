96fb3a98564824cf39d9cc22b6d38607595d90c5
imguerrilla <imguerrilla21@gmail.com>
2026-08-29 19:50:57 +0700

    Fix GitHub Actions JVM memory limit and prevent circular project evaluation

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,7 +16,9 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
-    project.evaluationDependsOn(":app")
+    if (project.name != "app") {
+        project.evaluationDependsOn(":app")
+    }
 }
 
 tasks.register<Delete>("clean") {
